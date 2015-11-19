9eb8b34ace7e33a05c6e0827c8e84f24fe2b8819
William <williamokano@gmail.com>
2015-11-19 13:13:29 -0200

    Added some tests

---
+++ tests/CarbonTest.php
@@ -0,0 +1,14 @@
+<?php
+
+use Carbon\Carbon;
+
+class CarbonTest extends PHPUnit_Framework_TestCase
+{
+    public function testDate()
+    {
+        $now = date("Y-m-d H:i:s");
+        $carbon = new Carbon();
+
+        $this->assertEquals($now, (string)$carbon, sprintf("%s - %s : datas diferentes!", $now, (string)$carbon));
+    }
+}
